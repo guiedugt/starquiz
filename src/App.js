4506233561c915b8a2584c6a31b54aab6dd2c11b
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-02-21 16:59:01 -0300

    Add hot reloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 import { Router } from 'react-router-dom'
 import history from './routes/history'
 import Routes from './routes/Routes'
@@ -10,4 +11,4 @@ const App = () => (
   </Router>
 )
 
-export default App
+export default hot(module)(App)
