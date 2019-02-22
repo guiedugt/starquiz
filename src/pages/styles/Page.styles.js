b57425c6298753f8e34b3a7a772138c7da03f93a
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-02-22 16:30:12 -0300

    Add Page styles

---
+++ src/pages/styles/Page.styles.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+const pageStyle = css`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 100%;
+  padding: 0.75rem;
+  margin: 0 auto;
+`
+
+const homePageStyle = blur => css`
+  ${pageStyle};
+  ${blur && 'filter: blur(.5rem)'};
+`
+
+const containerStyle = css`
+  width: 100%;
+  height: 100%;
+  padding: 0 1rem;
+`
+
+export const StyledHomePage = props => (
+  <section css={homePageStyle(props.blur)} {...props} />
+)
+
+export const Container = props => (
+  <div css={containerStyle} {...props} />
+)
