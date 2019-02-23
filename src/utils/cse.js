38caa7ece16a85dcf4eb8b647e21ad868376c531
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-02-23 00:28:27 -0300

    Add cse service

---
+++ src/utils/cse.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+const { NODE_ENV, GOOGLE_CX_ID, GOOGLE_API_KEY } = process.env
+
+const apiHosts = {
+  'production': 'https://www.googleapis.com',
+  'staging': 'https://www.googleapis.com',
+  'demo': 'https://www.googleapis.com',
+  'development': 'https://www.googleapis.com'
+}
+
+const apiHost = NODE_ENV ? apiHosts[NODE_ENV] : apiHosts['development']
+
+const instance = axios.create({
+  baseURL: `${apiHost}/customsearch/v1`
+})
+
+instance.defaults.params = {
+  cx: GOOGLE_CX_ID,
+  searchType: 'image',
+  imgSize: 'medium',
+  start: 1,
+  num: 1,
+  key: GOOGLE_API_KEY
+}
+
+export const mapImages = res => {
+  return res.data.items.map(item => ({
+    url: item.link,
+    height: item.image.height,
+    width: item.image.width
+  }))
+}
+
+export const getRandomImage = images => {
+  const image = images[Math.floor(Math.random() * images.length)]
+  return image
+}
+
+export default instance
