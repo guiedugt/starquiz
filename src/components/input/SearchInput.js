c0c4369fe31f45a3dbe15e533b7d0adffef58a18
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-02-23 00:34:04 -0300

    Enhance SearchInput (submission)

--- src/components/input/SearchInput.js
+++ src/components/input/SearchInput.js
@@ -1,32 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Form, Input } from 'antd'
 import { StyledSearchInput } from './styles/Input.styles'
 
-export const SearchInput = ({
-  onSearch,
-  error
-}) => {
-  return (
-    <StyledSearchInput>
-      <Form.Item
-        hasFeedback
-        help={error}
-        validateStatus={error && 'error'}
-      >
-        <Input.Search
-          onSearch={input => window.alert(input)}
-          placeholder='My name is ...'
-          enterButton
-        />
-      </Form.Item>
-    </StyledSearchInput>
-  )
-}
+export class SearchInput extends Component {
+  static propTypes = {
+    onSearch: PropTypes.func.isRequired,
+    error: PropTypes.string
+  }
 
-SearchInput.propTypes = {
-  onSearch: PropTypes.func.isRequired,
-  error: PropTypes.string
+  state = {
+    input: ''
+  }
+
+  render () {
+    const {
+      error
+    } = this.props
+
+    return (
+      <StyledSearchInput>
+        <Form.Item
+          hasFeedback
+          help={error}
+          validateStatus={error && 'error'}
+        >
+          <Input.Search
+            onChange={this.handleChange}
+            onSearch={this.handleSearch}
+            placeholder='My name is ...'
+            enterButton
+          />
+        </Form.Item>
+      </StyledSearchInput>
+    )
+  }
+
+  handleChange = input => {
+    this.setState({ input })
+  }
+
+  handleSearch = input => {
+    this.setState({ input: '' })
+    this.props.onSearch(input)
+  }
 }
 
 export default SearchInput
