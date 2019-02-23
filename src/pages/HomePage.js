229abaff134232a3c7518da9b9cbfa9192900635
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-02-23 15:51:50 -0300

    Add HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Header from '../components/header/Header'
+import RulesModal from '../components/modal/RulesModal'
+import CharacterList from '../components/list/CharacterList'
+import { StyledHomePage } from './styles/Page.styles'
+import { fetchCharacters } from '../store/characters/actions'
 
-export const HomePage = () => <h1>HomePage</h1>
+export class HomePage extends Component {
+  componentDidMount () {
+    const { characters, fetchCharacters } = this.props
+    characters.length === 0 && fetchCharacters()
+  }
 
-export default HomePage
+  render () {
+    const {
+      fetchCharacters,
+      characters,
+      loading
+    } = this.props
+
+    return (
+      <StyledHomePage blur={0}>
+        <Header time={120} score={300} />
+        <RulesModal visible={false} />
+        <CharacterList
+          scrollParentRef={this.page}
+          data={characters}
+          loading={loading}
+          fetch={fetchCharacters}
+        />
+      </StyledHomePage>
+    )
+  }
+}
+
+const mapStateToProps = ({ characters }) => ({
+  characters: characters.items,
+  loading: characters.loading
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({
+    fetchCharacters
+  }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
