84e60cd2076f2a80fc588dc3faa3678087b8ad3b
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-02-23 00:35:03 -0300

    Enhance PublicPage (add fetchCharacters)

--- src/pages/PublicPage.js
+++ src/pages/PublicPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import StarWarsCrawl from '../components/crawl/StarWarsCrawl'
 import OpeningTitle from '../components/title/OpeningTitle'
 import SkipButton from '../components/button/SkipButton'
 import starWarsIntro from '../assets/star-wars-intro.mp3'
 import history from '../routes/history'
+import { fetchCharacters } from '../store/characters/actions'
 
 export class PublicPage extends Component {
   componentDidMount () {
+    this.props.fetchCharacters()
     setTimeout(() => history.push('/home'), 27000)
   }
 
@@ -30,4 +34,9 @@ export class PublicPage extends Component {
   }
 }
 
-export default PublicPage
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({
+    fetchCharacters
+  }, dispatch)
+
+export default connect(null, mapDispatchToProps)(PublicPage)
